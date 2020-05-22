2ca4e0a22f8f0be9bf5885db87af16d567d4fbf8
Beatriz Ferreira dos Santos <BFSantos@raiadrogasil.com>
2020-05-22 20:41:44 -0300

    alterando a estrutura da requisição AJAX

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
-var xhr = new XMLHttpRequest();
+// REQUISIÇÃO JAVASCRIPT PURO
+// var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'http://localhost:3001/products');
-xhr.send(null);
+// xhr.open('GET', 'http://localhost:3001/products');
+// xhr.send(null);
+
+// xhr.onreadystatechange = function(){
+//     if (xhr.readyState === 4){
+//         console.log(JSON.parse(xhr.responseText));
+//     }
+// }
+//----------------------------------------------------------------//
+
+var minhaPromise = function(){
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'http://localhost:3001/products');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function(){
+          if (xhr.readyState === 4){
+           //console.log(JSON.parse(xhr.responseText));
+           if (xhr.status === 200){
+               resolve(JSON.parse(xhr.responseText));
+           } else{
+               reject('Erro na requisição');
+           }
+           }
+         }
+    });
+
+    }
