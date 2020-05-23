5715ebacf983dee490421d69587df1e5a9b5b245
Beatriz Ferreira dos Santos <BFSantos@raiadrogasil.com>
2020-05-23 01:17:53 -0300

    utilizando axios para fazer requisição AJAX

--- main.js
+++ main.js
@@ -12,33 +12,42 @@
 //----------------------------------------------------------------//
 
 // FAZENDO UMA REQUISIÇÃO DO TIPO GET QUE RETORNA UM OBJETO JSON 
-var minhaPromise = function(){
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
+// var minhaPromise = function(){
+//     return new Promise(function(resolve, reject){
+//         var xhr = new XMLHttpRequest();
 
-        xhr.open('GET', 'http://localhost:3001/products');
-        xhr.send(null);
+//         xhr.open('GET', 'http://localhost:3001/products');
+//         xhr.send(null);
 
-        xhr.onreadystatechange = function(){
-          if (xhr.readyState === 4){
-           //console.log(JSON.parse(xhr.responseText));
-           if (xhr.status === 200){
-               resolve(JSON.parse(xhr.responseText));
-           } else{
-               reject('Erro na requisição');
-           }
-           }
-         }
-    });
+//         xhr.onreadystatechange = function(){
+//           if (xhr.readyState === 4){
+//            //console.log(JSON.parse(xhr.responseText));
+//            if (xhr.status === 200){
+//                resolve(JSON.parse(xhr.responseText));
+//            } else{
+//                reject('Erro na requisição');
+//            }
+//            }
+//          }
+//     });
 
-    }
+//     }
 
 // CHAMANDO FUNÇÃO E TRATANDO O SUCESSO DA REQUISIÇÃO '.THEN ' ||  ERRO NA REQUISIÇÃO '.CATCH'
- minhaPromise()
- .then(function(response){
-     console.log(response);
- })
- .catch(function(error){
-     console.log(error);
- })
- ;
+//  minhaPromise()
+//  .then(function(response){
+//      console.log(response);
+//  })
+//  .catch(function(error){
+//      console.log(error);
+//  })
+//  ;
+
+
+axios.get('http://localhost:3001/products')
+    .then(function(response){
+         console.log(response);
+     })
+     .catch(function(error){
+         console.log('Erro na requisição AJAX com Axios');
+     });
